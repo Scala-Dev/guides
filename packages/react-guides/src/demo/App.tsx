cb705e16ac7ebd04073bb19c7905287ca3a92a2a
Dave Mieloch <davidmieloch@gmail.com>
2021-03-03 14:53:28 -0500

    add lock guides button to demo

--- packages/react-guides/src/demo/App.tsx
+++ packages/react-guides/src/demo/App.tsx
@@ -10,6 +10,7 @@ export default class App extends Component<{}> {
     public state = {
         zoom: 72,
         unit: 1,
+        lockGuides: false,
     };
     private scene: Scene = new Scene();
     // private editor!: Editor;
@@ -25,6 +26,7 @@ export default class App extends Component<{}> {
                     type="horizontal"
                     zoom={this.state.zoom}
                     unit={this.state.unit}
+                    lockGuides={this.state.lockGuides}
                     snapThreshold={5}
                     textFormat={v => `${v}in`}
                     snaps={[1, 2, 3]}
@@ -49,6 +51,7 @@ export default class App extends Component<{}> {
             <div className="ruler vertical">
                 <Guides ref={ref(this, "guides2")}
                     type="vertical"
+                    lockGuides={this.state.lockGuides}
                     zoom={this.state.zoom}
                     unit={this.state.unit}
                     snaps={[100, 200, 400]}
@@ -56,6 +59,15 @@ export default class App extends Component<{}> {
                     displayDragPos={true}
                     onChangeGuides={({ guides }) => {
                         console.log("vertical", guides);
+                    }}
+                    onDragStart={e => {
+                        console.log("dragStart", e);
+                    }}
+                    onDrag={e => {
+                        console.log("drag", e);
+                    }}
+                    onDragEnd={e => {
+                        console.log("dragEnd", e);
                     }}
                 />
             </div>
@@ -100,6 +112,7 @@ export default class App extends Component<{}> {
                 <p className="description">A React Guides component that can draw ruler and manage guidelines.</p>
                 <div className="buttons">
                     <a href="https://github.com/daybrush/guides/tree/master/packages/svelte-guides" target="_blank">Download</a>
+                    <a onClick={() => this.setState({ lockGuides: !this.state.lockGuides })}>{this.state.lockGuides ? 'Unlock ' : 'Lock '}Guides</a>
                 </div>
             </div>
         </div>
