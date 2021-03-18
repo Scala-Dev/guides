d53857e2c2f61a6bf10c9bfe02fc71cf8ef20401
Po-Tsun Chen <po.chen@scala.com>
2021-03-18 15:10:28 -0400

    make dblclick optional

--- packages/react-guides/src/react-guides/Guides.tsx
+++ packages/react-guides/src/react-guides/Guides.tsx
@@ -29,6 +29,7 @@ export default class Guides extends React.PureComponent<GuidesProps, GuidesState
         defaultGuides: [],
         lockGuides: false,
         showGuides: true,
+        deleteOnDblclick: true,
     };
     public state: GuidesState = {
         guides: [],
@@ -312,8 +313,9 @@ export default class Guides extends React.PureComponent<GuidesProps, GuidesState
             let isChange = false;
 
             guides = [...guides];
+            const deleteOnDblclick = this.props.deleteOnDblclick && isDouble;
 
-            if (isDouble || guidePos < this.scrollPos) {
+            if (deleteOnDblclick || guidePos < this.scrollPos) {
                 if (lockGuides && (lockGuides === true || lockGuides.indexOf("remove") > -1)) {
                     return;
                 }
